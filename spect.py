f0d68d4cc7573396800969ca8c14c5323ce0caae
jeff gerhard <gerhardj@gmail.com>
2016-08-31 21:16:31 -0400

    editing the html header

--- spect.py
+++ spect.py
@@ -42,29 +42,42 @@ def buildHTML(f, s):
         text = z.read()
         lines = text.splitlines()
     k, text = parseFile(lines)
+    if s == 'blog':
+        k['section'] = blogtitle
     # print(k)
     # print(markdown.markdown(text, extensions=['smarty']))
     htm += header(**k)
     htm += '<body>\n'
     htm += markdown.markdown(text, extensions=['smarty'])
+    htm += '</body></html>
     return htm
 
 
 def header(**kwargs):
-    htm = '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n'
-    htm += '<meta http-equiv="x-ua-compatible" content="ie=edge">\n'
-    htm += '<meta name="viewport" content="width=device-width,'
-    htm += 'initial-scale=1">\n<title>'
-    htm += kwargs['title']
-    htm += '</title>\n<link rel="stylesheet" href="..\styles\styles.css">\n'
-    htm += '<meta name="generator" content="Spect">\n'
+    htm = '<!DOCTYPE html>\n'
+    htm += '<html>\n'
+    htm += '<head>\n'
+    htm += '\t<meta charset="utf-8">\n'
+    htm += '\t<meta http-equiv="x-ua-compatible" content="ie=edge">\n'
+    htm += '\t<meta name="viewport" content="width=device-width,'
+    htm += 'initial-scale=1">\n'
+    htm += '\t<title>'
+    if kwargs['title']:
+        htm += kwargs['title'] + ' : '
+    if kwargs['section']:
+        htm += kwargs[section']
+    else:
+        htm += 'jeffgerhard.com'
+    htm += '</title>\n
+    htm += '\t<link rel="stylesheet" href="..\styles\styles.css">\n'
+    htm += '\t<meta name="generator" content="spect https://github.com/jeffgerhard/spect">\n'
     htm += '</head>\n'
     return htm
 
 localdir = r'C:\Users\gerhardj\Dropbox\__websites\python\testsite'
 mddir = os.path.join(localdir, 'md')
 wdir = os.path.join(localdir, 'www')
-
+blogtitle = 'introspect'
 secs = get_immediate_subdirectories(mddir)
 
 for s in secs:
