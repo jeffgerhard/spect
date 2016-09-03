035282ee8c44bae7f7660987160f7987eb94cd5b
jeff gerhard <gerhardj@gmail.com>
2016-09-02 20:38:41 -0400

    just some more development; switched to pure .md file

--- spect.py
+++ spect.py
@@ -5,7 +5,7 @@ Created on Wed Aug 31 10:15:38 2016
 @author: gerhardj
 """
 
-import markdown
+import markdown as m
 import os
 
 
@@ -15,45 +15,35 @@ def get_immediate_subdirectories(a_dir):
             if os.path.isdir(os.path.join(a_dir, name))]
 
 
-def get_jfiles(x):
+def get_mdfiles(x):
     a_dir = os.path.join(mddir, x)
     return [name for name in os.listdir(a_dir)
-            if name[-1:] == 'j']
-
-
-def parseFile(lines):
-    kwargs = {}
-    text = []
-    for l in lines:
-        if l[0:2] == '$$':
-            y, z = l.split(":")
-            v = y[2:]
-            kwargs[v] = z
-        else:
-            text.append(l)
-    md = '\n'.join(text)
-    return kwargs, md
+            if name[-2:] == 'md']
 
 
 def buildHTML(f, s):
     h = os.path.join(mddir, s, f)
     htm = ''
     with open(h, mode='r', encoding='utf-8') as z:
-        text = z.read()
-        lines = text.splitlines()
-    k, text = parseFile(lines)
+        t = z.read()
+#    k, text = parseFile(lines)
+    text = md.convert(t)
+    k = md.Meta
     if s == 'blog':
-        k['section'] = blogtitle
-    # print(k)
+        k['section'] = [blogtitle]
+    else:
+        k['section'] = [s]
+    print(k)
     # print(markdown.markdown(text, extensions=['smarty']))
-    htm += header(**k)
+    htm += head(**k)
     htm += '<body>\n'
-    htm += markdown.markdown(text, extensions=['smarty'])
+    htm += header(**k)
+    htm += md.convert(text)
     htm += '</body></html>'
     return htm
 
 
-def header(**kwargs):
+def head(**kwargs):
     htm = '<!DOCTYPE html>\n'
     htm += '<html>\n'
     htm += '<head>\n'
@@ -62,26 +52,42 @@ def header(**kwargs):
     htm += '\t<meta name="viewport" content="width=device-width,'
     htm += 'initial-scale=1">\n'
     htm += '\t<title>'
-    if kwargs['title']:
-        htm += kwargs['title']
-    if kwargs['section']:
-        htm += ' : ' + kwargs['section']
+    if 'title' in kwargs:
+        htm += str(kwargs['title'][0])
+    if 'section' in kwargs:
+        htm += ' : ' + kwargs['section'][0]
     else:
         htm += '&mdash; jeffgerhard.com'
     htm += '</title>\n'
-    htm += '\t<link rel="stylesheet" href="..\styles\styles.css">\n'
-    htm += '\t<meta name="generator" content="spect https://github.com/jeffgerhard/spect">\n'
+    stz = ['styles']
+    if 'styles' in kwargs:
+        for s in kwargs['styles']:
+            stz.append(s)
+    for st in stz:
+        htm += '\t<link rel="stylesheet" href="../styles/'
+        htm += st
+        htm += '.css">\n'
+    htm += '\t<meta name="generator" content="https://github.com/jeffgerhard/spect">\n'
     htm += '</head>\n'
     return htm
 
-localdir = r'C:\Users\gerhardj\Dropbox\__websites\python\testsite'
+def header(**kwargs):
+    htm = '<header>\n'
+    htm += '\t'
+    return htm
+
+md = m.Markdown(extensions=['meta', 'smarty'])
+# think about how to make the local md files add to the extension list
+
+# localdir = r'C:\Users\gerhardj\Dropbox\__websites\python\testsite'
+localdir = r'C:\Users\J\Dropbox\__websites\python\testsite'
 mddir = os.path.join(localdir, 'md')
 wdir = os.path.join(localdir, 'www')
 blogtitle = 'introspect'
 secs = get_immediate_subdirectories(mddir)
 
 for s in secs:
-    files = get_jfiles(s)
+    files = get_mdfiles(s)
     for f in files:
         x = buildHTML(f, s)
         print(x)
